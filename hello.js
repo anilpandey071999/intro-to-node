ae2ee4b4293751ae6e7812d63cdcf10b7b64d661
Anil Pandey <anilpandey071999@gmail.com>
2020-03-26 14:23:11 +0530

    added procfile

--- hello.js
+++ hello.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var url = require('url');
 var fs =require('fs');
+const PORT = process.env.PORT || 5000
 
 http.createServer(function(req,res){
 	var q = url.parse(req.url,true);
@@ -24,4 +25,4 @@ http.createServer(function(req,res){
 		//}
 		
 	})
-}).listen(8080);
+}).listen(PORT);
